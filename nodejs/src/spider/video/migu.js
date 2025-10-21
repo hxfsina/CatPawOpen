7cad7736ca8110f23f47adb260ba6795581c7333
hxfsina <hxfkof@sina.com.cn>
2025-10-21 09:25:24 +0800

    Update migu.js

--- nodejs/src/spider/video/migu.js
+++ nodejs/src/spider/video/migu.js
@@ -31,13 +31,13 @@ async function home(_inReq, _outResp) {
 
     const data = response.data.data;
     const classes = data.class || [];
-    const filters = data.filters || [];  
+    const filterses = data.filters || [];  
 
     //console.log(`成功获取 ${classes.length} 个分类`);
     
     return {
       class: classes
-      filters: filters
+      filters: filterses
     };
   } catch (error) {
     //console.error('获取首页分类失败:', error.message);
